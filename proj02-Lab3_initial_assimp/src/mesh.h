03e4904a85a56122da5d136b92092ccd03236682
Joadlaen <jackcullenmurphy1999@gmail.com>
2025-10-23 13:57:56 +0100

    did something

---
+++ proj02-Lab3_initial_assimp/src/mesh.h
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+
+#include <glad/glad.h>
+
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/transform.hpp>
+
+class Mesh {
+
+protected:
+    // array of vertices and normals
+    std::vector< glm::vec3 > vertices;
+
+    // triangle vertex indices
+    std::vector< unsigned int > indices;
+
+    std::vector<GLuint> buffers;
+
+public:
+    void loadModel(std::string path);
+    void draw();
+};
