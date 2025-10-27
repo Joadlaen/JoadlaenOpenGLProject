a397e92b03d040e8e83543b9d707dd7eddea3bf6
Joadlaen <jackcullenmurphy1999@gmail.com>
2025-10-27 15:58:15 +0000

    started using indices

--- home_project/src/main.cpp
+++ home_project/src/main.cpp
@@ -4,39 +4,70 @@
 
 void initTriangle()
 {
+    //GLfloat verts[] = {
+    ////-1.0f, -1.0f, // v1
+    ////1.0f, -1.0f, // v2
+    ////0.0f, 1.0f, // v3
+
+    ////1.0f, -0.5f, // v2
+    ////1.0f, 1.0f, // v4
+    ////0.5f, 1.0f, // v3
+    //    -1.0f, 1.0f,  0.0f,// v0
+    //    -1.0f, -1.0f, 0.0f,// v1
+    //    1.0f, -1.0f,  0.0f, // v2
+    //    1.0f, 1.0f,   0.0f,// v3
+    //};
+
+    //// indices of two triangles
+    //GLuint indices[] = { 0, 1, 2, 2, 3, 0 };
+
+        // two triangles : vertex data
     GLfloat verts[] = {
-    -1.0f, -1.0f,
-    1.0f, -1.0f,
-    0.0f, 0.5f,
+        -1.0f, 1.0f,  0.0f,// v0
+        -1.0f, -1.0f, 0.0f,// v1
+        1.0f, -1.0f,  0.0f, // v2
+        1.0f, 1.0f,   0.0f,// v3
     };
 
-    GLuint bufID;
+    // indices of two triangles
+    GLuint indices[] = { 0, 1, 2, 2, 3, 0 };
+
+    /*GLuint bufID;
     glGenBuffers(1, &bufID);
     glBindBuffer(GL_ARRAY_BUFFER, bufID);
 
     glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STATIC_DRAW);
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0);*/
+
+    // create index buffer
+    GLuint idxBufID;
+    glGenBuffers(1, &idxBufID);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, idxBufID);
+
+    // set buffer data for triangle index
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 }
 
 void drawTriangle()
 {
-    glColor3f(0.0f, 0.0f, 0.0f);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glColor3f(1.0f, 0.0f, 0.0f);
+    // glDrawArrays(GL_TRIANGLES, 0, 6);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 }
 
 
-void main()
+int main()
 {
  GLFWwindow *window;
 
     // GLFW init
     if (!glfwInit())
     {
-
+        return -1;
     }
 
-    window = glfwCreateWindow(500, 350, "Azure Renderer", NULL, NULL);
+    window = glfwCreateWindow(750, 400, "Azure Renderer", NULL, NULL);
     glfwMakeContextCurrent(window);
 
     // loading glad
@@ -44,6 +75,7 @@ void main()
     {
         std::cout << "Couuldn't load opengl" << std::endl;
         glfwTerminate();
+        return -1;
     }
 
     initTriangle();
@@ -66,25 +98,5 @@ void main()
 
     glfwTerminate();
 
-    // GLAD init
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
-        std::cout << "Couuldn't load opengl" << std::endl;
-        glfwTerminate();
-    }
-
-    // set background colour
-    glClearColor(0.25f, 0.5f, 1.0f, 1.0f);
-
-    // rendering loop
-    while (!glfwWindowShouldClose(window))
-    {
-        glfwPollEvents();
-
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        glfwSwapBuffers(window);
-    }
-
-    glfwTerminate();
+    return 0;
 }
